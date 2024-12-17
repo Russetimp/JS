1f0638be85e7c12a3af364eb808117222c60c293
Russetimp <russetimp@gmail.com>
2024-12-17 17:33:09 +0300

    17.12.2024

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-"use strict";
-// const greeting = 15
-// const message = `${greeting}+${greeting}`
-// console.log(message)
-
-// // Преобразуются в false
-// console.log ( Boolean (0))
-// console.log ( Boolean (NaN))
-// console.log ( Boolean (''))
-// console.log ( Boolean (null))
-// console.log ( Boolean (undefined))
-// // Все остальные значения - в true
-
-// const year = 3000
-// const message = year === 2024
-//  ? 'Начинаю учиться!'
-//  : year === 3000
-//     ? 'Нейросети победили'
-//     : 'Неизвестный год:/'
-
-// console.log(message);
-
-const globalMessage = "Привет";
-
-function logMessage(message = "Мяу", count) {
-  const messageFormatted = `(((${message})))`;
-
-  for (let i = 0; i < count; i++) {
-    console.log(messageFormatted);
-  }
-}
-
-logMessage(undefined, 3);
-console.log(globalMessage);
- 
+const myName = 'Илья'
+myName = ''
+console.log(myName)
