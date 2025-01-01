d36315b3f4e512fd9833481f6fa2dbd17779c36c
Russetimp <russetimp@gmail.com>
2025-01-01 20:00:33 +0300

    Modern JavaScript Tutorial - start (1.1-2.4)

--- script.js
+++ script.js
@@ -1,55 +1,12 @@
-// const objectA = {
-//     a: 10,
-//     b: 20
-// }
-// const objectB  = objectA
-// console.log(objectA)
-// console.log(objectB)
-// objectA.f = 30
-// objectA.f = 0
-// console.log(objectA)
-// console.log(objectB)
+"use strict"
 
-// // !!!ОБЪЯВЛЕНИЕ ФУНКЦИИ ЧЕРЕЗ CONST!!! 1:35:25 (function допускает присваивание нового типа переменной, const-нет; имеет другую запись)
-// function a () {
-//     console.log('Hey there')
-// }
-// a()
-// a = 10
-// a()
-// const b = () => {
-//     console.log('Hey you')
-// }
-// b()
+// 2.4 Переменные
+// Задача_1
+let admin;
+let myName = "Джон";
+admin = myName;
+alert(admin);
+console.log(myName);
 
-// // !!!ОБЪЕКТЫ!!! 1:48:50
-// const myCity = {
-//     country:'Russia',
-//     city: 'Moscow',
-//     popular: true
-// }
-// console.log('myCity.country:',myCity.country)
-// console.log('myCity.city:', myCity.city)
-// myCity.city = 'World'
-// console.log('Изменили свойство `city`:', myCity.city)
-// myCity.size = 'big'
-// console.log('Удалили свойство `big`:',myCity)
-// delete myCity.popular
-// console.log('Удалили свойство `popular`:', myCity)
-// // !!!Использование переменных!!!
-// const userName = 'Илья'
-// const age = 25
-// const userProfile = {
-//     userName: userName,
-//     age: age,
-//     height: 170
-// }
-// console.log(userProfile)
-// const userName = 'Илья'
-// const age = 25
-// const userProfile2 = {
-//     userName,
-//     age,
-//     height: 170
-// }
-// console.log(userProfile2)
+planetEarth
+currentSiteUser
