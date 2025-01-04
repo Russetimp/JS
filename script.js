1af85e2eb99a9c799f5292bf25eda4ac31f4eff7
Russetimp <russetimp@gmail.com>
2025-01-04 21:10:31 +0300

    4.1-4.8 (04.01.2025)

--- script.js
+++ script.js
@@ -48,18 +48,18 @@
 // let number = prompt ("Введите число больше 100", "");
 // if (number > 100 || number == null) {
 //     alert (number);
-//     break;} 
+//     break;}
 // }
 
 // // 2.15 Функции
 // let from = "Аня";
 // console.log( from );
 //   function showMessage1(text) {
-//     from = '*' + from + '*'; 
+//     from = '*' + from + '*';
 //     console.log( from + ': ' + text );
 //   }
-//   showMessage1("Привет"); 
-//   console.log( from ); 
+//   showMessage1("Привет");
+//   console.log( from );
 
 // function ask(question, yes, no) {
 //     if (confirm(question)) yes()
@@ -70,3 +70,78 @@
 //     function() { alert("Вы согласились."); },
 //     function() { alert("Вы отменили выполнение."); }
 //   );
+
+// // 4.1 Объекты.Задача.Сумма свойств объекта (Можно и без функции)
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
+// // let sum = function () {
+// //     let sum = 0;
+// //     for (let key in salaries) {
+// //         sum = sum + salaries[key];
+// //     };
+// //     return sum;
+// // };
+// // salaries.sum = sum()
+// // console.log(salaries.sum);
+
+//     let sum = 0;
+//     for (let key in salaries) {
+//         sum += salaries[key];
+//     };
+//     console.log(sum);
+
+
+
+// // //   Задача "Умножаем все числовые свойства на 2"
+// // //   Можно избежать дополнительной вложенности из-за if
+// // let menu = {
+// //     width: 2.2,
+// //     height: 300,
+// //     title: "My menu"
+// //   };
+// //   function multiplyNumeric(obj) {
+// //     for (let key in obj) {
+// //         obj[key] = obj[key] * 2 || obj[key];
+// //     } 
+// //   }
+//   multiplyNumeric(menu);
+//   console.log(menu);
+// function multiplyNumeric(obj) {
+//     for (let key in obj) {
+//       if (typeof obj[key] == 'number') {
+//         obj[key] *= 2;
+//       }
+//     }
+//   }
+
+// // 4.4 Методы объекта, "this" Задачи
+// let calculator = {
+//     read () {
+//         this.a = +prompt ("назови а", 2)
+//         this.b = +prompt ("назови а", 2)
+//     },
+//     sum() {
+//         return this.a + this.b
+//     },
+//     mul() {
+//         return this.a * this.b;
+//       },
+//   };
+//   calculator.read();
+//   alert( calculator.sum() );
+//   alert( calculator.mul() );
+
+// // 4.5. Задача."Конструктор, оператор "new" Две функции - один объект'
+// let obj = {a:1};
+// function A() { return obj; }
+// function B() { return obj; }
+// let a = new A();
+// let b = new B();
+// console.log (obj);
+// console.log (a);
+// console.log (b); 
+// console.log (a == b);
+// console.log (a === b);
