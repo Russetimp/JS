4b67761d998475c20bbbee9ea15cc710a9214a47
Russetimp <russetimp@gmail.com>
2025-01-05 20:59:24 +0300

    5.1-5.4 (05.01.2025)

--- script.js
+++ script.js
@@ -93,8 +93,6 @@
 //     };
 //     console.log(sum);
 
-
-
 // // //   Задача "Умножаем все числовые свойства на 2"
 // // //   Можно избежать дополнительной вложенности из-за if
 // // let menu = {
@@ -105,7 +103,7 @@
 // //   function multiplyNumeric(obj) {
 // //     for (let key in obj) {
 // //         obj[key] = obj[key] * 2 || obj[key];
-// //     } 
+// //     }
 // //   }
 //   multiplyNumeric(menu);
 //   console.log(menu);
@@ -142,6 +140,73 @@
 // let b = new B();
 // console.log (obj);
 // console.log (a);
-// console.log (b); 
+// console.log (b);
 // console.log (a == b);
 // console.log (a === b);
+
+// // 5.2 Числа Задача
+// function readNumber() {
+//     let num;
+//     do {
+//       num = prompt("Введите число", 0);
+//     } while ( !isFinite(num) );
+//     if (num === null || num === '') return null;
+//     return +num;
+//   }
+//   console.log(`Число: ${readNumber()}`);
+
+// function getRandomArbitrary(min, max) {
+//     let a;
+//     a = Math.random();
+//     console.log (a);
+//     a *= (max - min);
+//     console.log (a);
+//     a += min;
+//     console.log (a);
+//     return a;
+//   }
+//   getRandomArbitrary(1, 3);
+
+// function randomInteger(min, max) {
+//     // случайное число от min до (max+1)
+//     let rand = min + Math.random() * (max + 1 - min);
+//     return Math.floor(rand);
+//   }
+//   console.log ( randomInteger(1, 2) );
+
+// // 5.3 Строки Задача
+// function checkSpam(str) {
+// let strLower = str.toLowerCase();
+// console.log ( strLower.includes("viagra") || strLower.includes("xxx"))
+// }
+// checkSpam('buy ViAgRA now') ;
+// checkSpam('free xxxxx') ;
+// checkSpam("innocent rabbit");
+
+// function truncate(str, maxlength) {
+//   if (str.length > maxlength) {
+//     str = str.slice(0, maxlength - 1) + "...";
+//     console.log(str);
+//   } else {
+//     console.log(str);
+//   }
+// }
+// truncate("Вот, что мне хотелось бы сказать на эту тему:", 20);
+
+// // 5.4 Массивы. Подмассив наибольшей суммы
+// function getMaxSubSum(arr) {
+//     let maxSum = 0; // если элементов не будет - возвращаем 0
+//     for (let i = 0; i < arr.length; i++) {
+//       let sumFixedStart = 0;
+//       for (let j = i; j < arr.length; j++) {
+//         sumFixedStart += arr[j];
+//         maxSum = Math.max(maxSum, sumFixedStart);
+//       }
+//     }
+//     return maxSum;
+//   }
+//   console.log( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+//   console.log( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+//   console.log( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+//   console.log( getMaxSubSum([1, 2, 3]) ); // 6
+//   console.log( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
