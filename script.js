b2e843fe7f0219e723a507491b9966d85cc9e7d0
Russetimp <russetimp@gmail.com>
2025-02-21 21:55:17 +0300

    2.1-2.8 (21.02.2025)

--- script.js
+++ script.js
@@ -309,14 +309,14 @@
 // console.log(arr[1].name); // Маша
 // console.log(arr[2].name); // Петя
 
-// Перемешайте массив
-let arr = [1, 2, 3];
+// // Перемешайте массив
+// let arr = [1, 2, 3];
 
-shuffle(arr);
-// arr = [3, 2, 1]
+// shuffle(arr);
+// // arr = [3, 2, 1]
 
-shuffle(arr);
-// arr = [2, 1, 3]
+// shuffle(arr);
+// // arr = [2, 1, 3]
 
-shuffle(arr);
-// arr = [3, 1, 2]
+// shuffle(arr);
+// // arr = [3, 1, 2]
